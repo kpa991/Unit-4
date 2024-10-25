79e1bcb12fd6dc4e511456f3ad93fb22d3f7041e
kpa991 <pklimov41@gmail.com>
2024-10-25 21:10:50 +0300

    Попробовал реализовать код по разделении имени по буквам самостоятельно

--- Program.cs
+++ Program.cs
@@ -4,48 +4,19 @@
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Введите свое имя");
+            var name = Console.ReadLine();
 
-            int t = 0;
-            do
+            Console.WriteLine("Ваше имя по букввам: ");
+
+            foreach(var ch in name)
             {
+                Console.Write(ch + " ");
+            }
 
-                Console.WriteLine(t);
+            Console.WriteLine("Последняя буква вашего именни: {0}",name[name.Length - 1]);
 
-                Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
-                
-                switch (Console.ReadLine())
-                {
-                    case "red":
-                        Console.BackgroundColor = ConsoleColor.Red;
-                        Console.ForegroundColor = ConsoleColor.Black;
-
-                        Console.WriteLine("Your color is red!");
-                        break;
-
-                    case "green":
-                        Console.BackgroundColor = ConsoleColor.Green;
-                        Console.ForegroundColor = ConsoleColor.Black;
-
-                        Console.WriteLine("Your color is green!");
-                        break;
-
-                    case "cyan":
-                        Console.BackgroundColor = ConsoleColor.Cyan;
-                        Console.ForegroundColor = ConsoleColor.Black;
-
-                        Console.WriteLine("Your color is cyan!");
-                        break;
-
-                    default:
-                        Console.BackgroundColor = ConsoleColor.Yellow;
-                        Console.ForegroundColor = ConsoleColor.Red;
-
-                        Console.WriteLine("Your color is yellow!");
-                        break;
-                }
-                t++;
-            } while (t < 3);
-            
+            Console.ReadKey();
         }
     }
 }
