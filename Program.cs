2c3b80c0eacb3725174d0e814df5e1fb320093e8
kpa991 <pklimov41@gmail.com>
2024-10-25 12:07:41 +0300

    выполнил задание 4.1.17

--- Program.cs
+++ Program.cs
@@ -4,18 +4,32 @@
     {
         static void Main(string[] args)
         {
-            var a = 6;
-            var b = 7;
+            Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
 
-            if (!(a == b))
+            var color = Console.ReadLine();
+
+            if (color == "red")
             {
-                Console.WriteLine("Условие истинно");
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.ForegroundColor = ConsoleColor.Black;
+
+                Console.WriteLine("Your color is red!");
+            }
+
+            else if (color == "green")
+            {
+                Console.BackgroundColor = ConsoleColor.Green;
+                Console.ForegroundColor = ConsoleColor.Black;
+
+                Console.WriteLine("Your color is green!");
             }
             else
             {
-                Console.WriteLine("Условие ложно");
-            }
+                Console.BackgroundColor = ConsoleColor.Cyan;
+                Console.ForegroundColor = ConsoleColor.Black;
 
+                Console.WriteLine("Your color is cyan!");
+            }
         }
     }
 }
